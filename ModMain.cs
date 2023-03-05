a9ee6b5ab271cd7df597d22e0a90b5c72bdd63b1
Millipede406 <97822695+Millipede406@users.noreply.github.com>
2023-03-05 19:38:14 +0800

    Got everything working

--- ModMain.cs
+++ ModMain.cs
@@ -11,20 +11,29 @@ namespace FogMod
 {
     public class ModMain : MelonMod
     {
+        bool setDarknessSettings;
+        int currentScene;
+
         public override void OnSceneWasLoaded(int buildIndex, string sceneName)
         {
-            base.OnSceneWasLoaded(buildIndex, sceneName);
-
-            if(buildIndex == 1)
-            {
-                LoggerInstance.Msg($"Loaded main scene({sceneName}), setting darkness settings");
-                SetDarknessSettings();
-            }
+            currentScene = buildIndex;
         }
-
-        public void SetDarknessSettings()
+        public override void OnUpdate()
         {
-            DarknessSettings.Light = new DarknessSettings(new Color(0, 0, 0), 1, 0);
+            base.OnUpdate();
+
+            if(currentScene == 1)
+            {
+                if (!setDarknessSettings)
+                {
+                    if (!PatchQuest.Views.MainMenu.Instance.IsOpen)
+                    {
+                        Color darknessColor = new Color(0, 0, 0);
+                        DarknessSettings settings = new DarknessSettings(darknessColor, 1, 1);
+                        DarknessSettings.Light = settings;
+                    }
+                }
+            }
         }
     }
 }
